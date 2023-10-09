5da250ba6bd22b8250ede4687c396f98f6358bbb
alexandrebfreitas <71379827+alexandrebfreitas@users.noreply.github.com>
2023-10-09 20:50:20 -0300

    Project Created

---
+++ src/main/java/com/valorfuturoenergia/vfe/VfeApplication.java
@@ -0,0 +1,13 @@
+package com.valorfuturoenergia.vfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorfuturoenergia/vfe/VfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorfuturoenergia.vfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
