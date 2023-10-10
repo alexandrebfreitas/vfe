30d203372973470254053b170b3493665cb30d5b
alexandrebfreitas <71379827+alexandrebfreitas@users.noreply.github.com>
2023-10-09 22:44:49 -0300

    Banco de Teste e Annotations

--- src/main/java/com/valorfuturoenergia/vfe/entities/User.java
+++ src/main/java/com/valorfuturoenergia/vfe/entities/User.java
@@ -1,9 +1,15 @@
 package com.valorfuturoenergia.vfe.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="tbl_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
