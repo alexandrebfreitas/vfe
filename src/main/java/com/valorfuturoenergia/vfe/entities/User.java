47e3e2b25313ab7a6d01d0519ba96292c415edb0
alexandrebfreitas <71379827+alexandrebfreitas@users.noreply.github.com>
2023-10-14 18:55:25 -0300

    Order Status

--- src/main/java/com/valorfuturoenergia/vfe/entities/User.java
+++ src/main/java/com/valorfuturoenergia/vfe/entities/User.java
@@ -1,13 +1,18 @@
 package com.valorfuturoenergia.vfe.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name="users")
+@Table(name="tb_user")
 public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,7 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
-
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+    public User(){}
     public User(Long id, String name, String email,String phone, String password) {
         this.id = id;
         this.name = name;
@@ -64,6 +72,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -76,4 +88,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
--- src/main/java/com/valorfuturoenergia/vfe/repositories/OrderRepository.java
+++ src/main/java/com/valorfuturoenergia/vfe/repositories/OrderRepository.java
@@ -1,8 +1,8 @@
 package com.valorfuturoenergia.vfe.repositories;
 
+import com.valorfuturoenergia.vfe.entities.Order;
 import com.valorfuturoenergia.vfe.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
