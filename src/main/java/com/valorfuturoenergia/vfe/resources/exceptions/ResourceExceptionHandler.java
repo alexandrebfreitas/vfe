238fa5ebf41b04dcb47c6fc2ff12a9e63ccbe3e8
alexandrebfreitas <71379827+alexandrebfreitas@users.noreply.github.com>
2023-11-02 10:05:48 -0300

    Exception Handling - findById

--- src/main/java/com/valorfuturoenergia/vfe/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/valorfuturoenergia/vfe/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.valorfuturoenergia.vfe.resources.exceptions;
 
+import com.valorfuturoenergia.vfe.services.exceptions.DataBaseException;
 import com.valorfuturoenergia.vfe.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -12,9 +13,16 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardError> resourceNotFoun(ResourceNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBas(DataBaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
---
+++ src/main/java/com/valorfuturoenergia/vfe/services/exceptions/DataBaseException.java
@@ -0,0 +1,10 @@
+package com.valorfuturoenergia.vfe.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    private static final long serialVersion = 1L;
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+
+}
